523de76b8e9cfab8fb3edadb388e8c914360d94c
xinx1n <xin9142@qq.com>
2017-03-07 10:33:18 +0800

    初始化

---
+++ main.js
@@ -0,0 +1,3 @@
+function xxx(data){
+	alert('我是 student24 在 node-server1 目录下\nand\n' + data);
+}
---
+++ server.js
@@ -0,0 +1,43 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+//console.log(Object.keys(http))
+var port = process.env.PORT || 10024;
+
+var server = http.createServer(function(request, response){
+
+  var temp = url.parse(request.url, true);
+  var path = temp.pathname;
+  var query = temp.query;
+
+
+  switch(path){
+    case  '/':
+      var htmlString = fs.readFileSync('./index.html');
+      response.setHeader('Content-Type','text/html');
+      response.end(htmlString)		;
+      break;
+    case '/index.css':
+      var cssString = fs.readFileSync('./index.css');
+      response.setHeader('Content-Type','text/css');
+      response.end(cssString);
+      break;
+    case '/main.js':
+      var jsString = fs.readFileSync('./main.js');
+      response.setHeader('Content-Type','application/javascript');
+      response.end(jsString);
+      break;
+    case '/run.js':
+      var functionName = query.callback;
+      response.end(functionName + '("my password is yyy")');
+      break;
+    default:
+      response.end('404');
+      break;
+  }
+
+})
+
+server.listen(port);
+console.log('监听 10024 成功');
